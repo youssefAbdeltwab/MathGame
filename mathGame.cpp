e769d3145442f36a91a76f28168b6c47bcb9ad8b
youssefAbdeltwab <youssefabdeltwab190@gmail.com>
2024-05-28 12:23:56 +0300

    first commit

---
+++ mathGame.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include  "mathQ.h"
+using namespace  std;
+using  namespace mathGame;
+
+
+
+
+int main()
+{
+	//Seeds the random number generator in C++, called only once
+	
+	srand((unsigned)time(NULL));
+
+	StartGame();
+
+	return  0;
+}
